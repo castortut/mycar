1453a3973b229ad14d3b3f6dce890dcefd55454e
Eero Heinänen <eero.heinanen@hotmail.com>
2019-05-27 20:53:45 +0300

    Implement noise

--- augment_data.py
+++ augment_data.py
@@ -15,8 +15,16 @@ Options:
 import os
 import copy
 import argparse
+import random
 from donkeycar.parts.datastore import TubGroup, Tub
 from shutil import copyfile
+
+import numpy as np
+
+
+CAM_IMG_ARRAY = 'cam/image_array'
+USER_ANGLE = 'user/angle'
+USER_THROTTLE = 'user/throttle'
 
 
 def augment_single_record(record, args):
@@ -41,7 +49,7 @@ def augment_single_record(record, args):
     noise_records = list()
     for rec in augmented_records:
         for _ in range(args.noise):
-            noise_records.append(noise(rec))
+            noise_records.append(noise(rec, args.noise_amount))
 
     augmented_records += noise_records
 
@@ -72,28 +80,50 @@ def augment(tub_names, new_data_dir, args):
 
 def flip(record):
     flipped = copy.copy(record)
-    flipped['cam/image_array'] = record['cam/image_array'][:, ::-1, :]
-    flipped['user/angle'] = -record['user/angle']
+    flipped[CAM_IMG_ARRAY] = record[CAM_IMG_ARRAY][:, ::-1, :]
+    flipped[USER_ANGLE] = -record[USER_ANGLE]
     return flipped
 
 
-def noise(record):
-    return copy.copy(record)
+def noise(record, amount):
+    noised = copy.copy(record)
+    img = record[CAM_IMG_ARRAY]
+    noise_img = np.random.random_integers(-amount, amount, img.shape)
+    noised_img = clip_image_to_range(img + noise_img)
+    noised[CAM_IMG_ARRAY] = noised_img
+    return noised
 
 
 def shadow(record):
     shadowed = copy.copy(record)
-    shadowed['cam/image_array'] = record['cam/image_array']
+    img = record[CAM_IMG_ARRAY]
+
+
+
+    shadowed[CAM_IMG_ARRAY] = record[CAM_IMG_ARRAY]
     return copy.copy(record)
+
+
+def clip_image_to_range(img, low=0, high=255):
+    img[img > high] = high
+    img[img < low] = low
+    return img
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--flip', action='store_false')
-    parser.add_argument('--noise', action='store_const', const=0, default=0)
-    parser.add_argument('--shadow', action='store_const', const=0, default=0)
+    parser.add_argument('--tub', type=str, help='Path of tub, source data')
+    parser.add_argument('--dest', type=str, help='Path of tub, destination data. Directory is created, if it does not exist.')
+    parser.add_argument('--flip', action='store_true', help='Augmentation by flipping image left -> right')
+    parser.add_argument('--noise', type=int, default=0,
+                        help='Augmentation by adding noise to image. Give number of how many times noise is added (how many new records are created from one record)')
+    parser.add_argument('--noise_amount', type=int, default=10, help='Amount of noise to be added: by default 1.0 (range [0, 255])')
+    parser.add_argument('--shadow', type=int, default=0,
+                        help='Augmentation by adding shadow to image. Give number how many times shadow is added (how many new records are created from one record')
+    parser.add_argument('--sun', type=int, default=0,
+                        help='Augmentation by adding bright area to image. Give number how many times bright area is added (how many new records are created from one record')
 
     args = parser.parse_args()
 
-    augment('tub_small', 'tub2', args)
+    augment(args.tub, args.dest, args)
