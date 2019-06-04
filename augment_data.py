6293f935bb0d42edc1daf2c4012cf8992ea8e275
epe <eelispeltola@users.noreply.github.com>
2019-06-04 12:20:33 +0300

    Fix default destination name, add progress dots

--- augment_data.py
+++ augment_data.py
@@ -13,6 +13,7 @@ Options:
     --chaos          Add periodic random steering when manually driving
 """
 import os
+import sys
 import copy
 import argparse
 import random
@@ -66,13 +67,16 @@ def augment_single_record(record, args):
 
 
 def augment(tub_names, new_data_dir, args):
-    new_data_dir = os.path.expanduser(new_data_dir)
-    if new_data_dir == "aug":
-        new_data_dir_full = os.path.join(tubgroup.tubs[0].meta_path, new_data_dir)
-    else:
-        new_data_dir_full = new_data_dir
 
     tubgroup = TubGroup(tub_names)
+
+    if new_data_dir == "aug":
+        new_data_dir = os.path.expanduser(new_data_dir)
+        new_data_dir_full = os.path.join(os.path.dirname(tubgroup.tubs[0].path), new_data_dir)
+    else:
+        new_data_dir = os.path.expanduser(new_data_dir)
+        new_data_dir_full = new_data_dir
+
 
     # If tub directory does not exist, create directory
     if not os.path.exists(new_data_dir):
@@ -84,11 +88,20 @@ def augment(tub_names, new_data_dir, args):
 
     new_tub = Tub(new_data_dir_full)
 
+    i=0
+    div_last=0
     for tub in tubgroup.tubs:
         for ix in tub.get_index(shuffled=False):
             record = tub.get_record(ix)
             for augmented_record in augment_single_record(record, args):
                 new_tub.put_record(augmented_record)
+                i=i+1
+                if i==1000:
+                    sys.stdout.write('.')
+                    sys.stdout.flush()
+                    i=0
+    sys.stdout.write("\nFinished augmenting all records to %." % (new_data_dir_full) )
+    sys.stdout.flush()
 
 
 def flip(record):
@@ -165,7 +178,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--tub', type=str, help='Path of tub, source data')
-    parser.add_argument('--dest', type=str, default="aug", help='Path of tub, destination data. Directory is created, if it does not exist.')
+    parser.add_argument('--dest', type=str, default="aug", help='Path of tub, destination data. Directory is created, if it does not exist. By default: tubdir/../aug')
     parser.add_argument('--flip', action='store_true', help='Augmentation by flipping image left -> right')
     parser.add_argument('--noise', type=int, default=0,
                         help='Augmentation by adding noise to image. Give number of how many times noise is added (how many new records are created from one record)')
